a3e7c12e4e3a4708b61f02b50fb8f8f85654a871
Ibrohim Meliqulov <ibrohim.meliqulov2003@gmail.com>
2026-02-23 09:21:07 +0500

    movies,movies_category

--- src/modules/movie_categories/movie_categories.controller.ts
+++ src/modules/movie_categories/movie_categories.controller.ts
@@ -1,6 +1,11 @@
-import { Controller } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { MovieCategoriesService } from './movie_categories.service';
+import { Role } from '@prisma/client';
+import { AuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RoleGuard } from 'src/common/guards/role.guard';
+import { Roles } from 'src/common/decorators/role.decorator';
+import { MoviesCategoriesDto, UpdateMoviesCategoriesDto } from './dto/create.dto';
 
 
 @ApiBearerAuth()
@@ -8,4 +13,66 @@ import { MovieCategoriesService } from './movie_categories.service';
 export class MovieCategoriesController {
     constructor(private readonly movieCategories: MovieCategoriesService) { }
 
-}
+    @ApiOperation({
+        summary: `${Role.Superadmin},${Role.Admin}`
+    })
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.Admin, Role.Superadmin)
+    @Get()
+    getAllMoviesCategories() {
+        return this.movieCategories.getAllMoviesCategories()
+    }
+
+
+    @ApiOperation({
+        summary: `${Role.Superadmin},${Role.Admin}`
+    })
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.Admin, Role.Superadmin)
+    @Get(":id")
+    getOneMovieCategory(
+        @Param("id", ParseIntPipe) id: number
+    ) {
+        return this.movieCategories.getOneMoviesCategory(id)
+    }
+
+    @ApiOperation({
+        summary: `${Role.Superadmin},${Role.Admin}`
+    })
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.Admin, Role.Superadmin)
+    @Post()
+    createMovieCategory(
+        @Body() payload: MoviesCategoriesDto,
+        @Req() req: Request
+    ) {
+        return this.movieCategories.createMovieCategory(payload, req['user'])
+    }
+
+
+    @ApiOperation({
+        summary: `${Role.Superadmin},${Role.Admin}`
+    })
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.Admin, Role.Superadmin)
+    @Put(":id")
+    updateMovieCategory(
+        @Param("id", ParseIntPipe) id: number,
+        @Body() payload: UpdateMoviesCategoriesDto,
+        @Req() req: Request
+    ) {
+        return this.movieCategories.updateMovieCategory(id, payload)
+    }
+
+    @ApiOperation({
+        summary: `${Role.Superadmin},${Role.Admin}`
+    })
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.Admin, Role.Superadmin)
+    @Delete(":id")
+    deleteMovieCategory(
+        @Param("id", ParseIntPipe) id: number
+    ) {
+        return this.movieCategories.deleteMoviesCategories(id)
+    }
+} 
--- src/modules/movie_categories/movie_categories.service.ts
+++ src/modules/movie_categories/movie_categories.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable, NotFoundException, Req } from '@nestjs/common';
 import { PrismaService } from 'src/core/database/prisma.service';
+import { MoviesCategoriesDto, UpdateMoviesCategoriesDto } from './dto/create.dto';
+import { Role } from '@prisma/client';
 
 @Injectable()
 export class MovieCategoriesService {
@@ -16,6 +18,82 @@ export class MovieCategoriesService {
     }
 
     async getOneMoviesCategory(id: number) {
+        const existMovieCategory = await this.prisma.movie_categories.findUnique({
+            where: { id }
+        })
+        if (!existMovieCategory) throw new NotFoundException()
 
+        return {
+            success: true,
+            data: existMovieCategory
+        }
+    }
+
+
+    async createMovieCategory(payload: MoviesCategoriesDto, current_user: { id: number, role: Role }) {
+
+        const existMovie = await this.prisma.movies.findUnique({
+            where: {
+                id: payload.movie_id
+            }
+        })
+        if (!existMovie) throw new NotFoundException("Movie not found")
+
+        const existCategory = await this.prisma.categories.findUnique({
+            where: {
+                id: payload.category_id
+            }
+        })
+        if (!existCategory) throw new NotFoundException("")
+
+        await this.prisma.movie_categories.create({
+            data: payload
+        })
+    }
+
+
+    async updateMovieCategory(id: number, payload: UpdateMoviesCategoriesDto) {
+        const existMovieCategory = await this.prisma.movie_categories.findUnique({
+            where: { id }
+        })
+        if (!existMovieCategory) throw new NotFoundException("Movie category not found")
+
+        if (payload.category_id) {
+            const existCategory = await this.prisma.categories.findUnique({
+                where: { id }
+            })
+            if (!existCategory) throw new NotFoundException("category not found")
+        }
+        if (payload.movie_id) {
+            const existMovie = await this.prisma.movies.findUnique({
+                where: { id }
+            })
+            if (!existMovie) throw new NotFoundException("Movie not found")
+        }
+
+        await this.prisma.movie_categories.update({
+            where: { id },
+            data: payload
+        })
+        return {
+            success: true,
+            message: "Movie category updated"
+        }
+    }
+
+
+
+    async deleteMoviesCategories(id: number) {
+        const existMovieCategory = await this.prisma.movie_categories.findUnique({
+            where: { id }
+        })
+        if (!existMovieCategory) throw new NotFoundException("Movie category not found")
+        await this.prisma.movie_categories.delete({
+            where: { id }
+        })
+        return {
+            success: true,
+            message: "Movie category deleted"
+        }
     }
 }
