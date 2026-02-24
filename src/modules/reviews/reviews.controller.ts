5a96e02bc6f21ef84408b39afd88df2d12a4d83f
Ibrohim Meliqulov <ibrohim.meliqulov2003@gmail.com>
2026-02-24 15:36:32 +0500

    reviews controller and service fully ended

--- src/modules/reviews/reviews.controller.ts
+++ src/modules/reviews/reviews.controller.ts
@@ -1,11 +1,88 @@
-import { Controller } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { ReviewsService } from './reviews.service';
+import { Role } from '@prisma/client';
+import { AuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RoleGuard } from 'src/common/guards/role.guard';
+import { Roles } from 'src/common/decorators/role.decorator';
+import { ReviewsDto, UpdateReviewsDto } from './dto/create.dto';
 
 @ApiBearerAuth()
 @Controller('reviews')
 export class ReviewsController {
-    constructor(private readonly reaviewService: ReviewsService) { }
+    constructor(private readonly reviewService: ReviewsService) { }
+
+
+    @ApiOperation({
+        summary: `$${Role.Superadmin},${Role.Admin}`
+    })
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.Admin, Role.Superadmin)
+    @Get()
+    getAllReviewz() {
+        return this.reviewService.getAllReviews()
+    }
+
+
+    @ApiOperation({
+        summary: `$${Role.Superadmin},${Role.Admin}`
+    })
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.Admin, Role.Superadmin)
+    @Get(":id")
+    getOwnReviews(
+        @Param("id", ParseIntPipe) id: number
+    ) {
+        return this.reviewService.getOneReview(id)
+    }
+
+    @ApiOperation({
+        summary: `$${Role.Superadmin},${Role.Admin}`
+    })
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.Admin, Role.Superadmin)
+    @Post()
+    createReview(
+        @Body() payload: ReviewsDto
+    ) {
+        return this.reviewService.createReview(payload)
+    }
+
+
+    @ApiOperation({
+        summary: `$${Role.Superadmin},${Role.Admin}`
+    })
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.Admin, Role.Superadmin)
+    @Put(":id")
+    updateReview(
+        @Param("id", ParseIntPipe) id: number,
+        @Body() payload: UpdateReviewsDto,
+        @Req() req: Request) {
+        return this.reviewService.updateReview(id, payload, req['user'])
+    } @ApiOperation({
+        summary: `$${Role.Superadmin},${Role.Admin}`
+    })
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.Admin, Role.Superadmin)
+    @Get()
+
+
+
+    @ApiOperation({
+        summary: `$${Role.Superadmin},${Role.Admin}`
+    })
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.Admin, Role.Superadmin)
+    @Delete(":id")
+    deleteReview(
+        @Param("id", ParseIntPipe) id: number,
+        @Req() req: Request
+    ) {
+        return this.reviewService.deleteReview(id, req['user'])
+    }
+
+
 
 
 
